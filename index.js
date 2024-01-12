4783c20161221dba45deef9602f9d77b052c30b9
Williams Hernández <hernandw@gmail.com>
2024-01-11 22:28:18 -0300

    :tada: archivos de la clase de node

---
+++ index.js
@@ -0,0 +1,24 @@
+let name = 'Juan Perez';
+
+console.log(name)
+
+name = 'Pedro Ramirez';
+
+console.log(name)
+
+let number = 18;
+
+console.log(number)
+
+/* const saludar = (name) => {
+    console.log(`Hola ¿como estas? ${name}`)
+} */
+
+const suma = (a, b) => {
+    return a + b
+}
+
+/* console.log(saludar(name)); */
+
+console.log(suma(15,38))
+console.log(suma(100,78))
